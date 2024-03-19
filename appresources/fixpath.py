30bd1fc659d4c326d9494530ec6f88f6114bfd0c
PratyushGR <pratyushrg@gmail.com>
2024-03-19 19:55:10 -0400

    updated fixpath

--- appresources/fixpath.py
+++ appresources/fixpath.py
@@ -7,9 +7,9 @@ fixpath_bp = Blueprint('fixpath', __name__)
 @fixpath_bp.route('/fixpath', methods = ['POST'])
 def fixpath():
     # Your home page logic here
-    input_text = request.form['input_text']+'/_internal'
+    input_text = request.form['input_text']
     if os.path.exists(input_text+'/appresources/meshnet/modelAE.json') ==  False:
-        print('Path invalid')
+        print('Path invalid',input_text+'/appresources/meshnet/modelAE.json')
         return render_template('fixpath.html')
     put_path(current_app.config['global_variables']['db'],input_text)
     return render_template('index.html')
