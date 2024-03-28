9d6fc83751878f72a055904c15c64b9fe9e3c65f
PratyushGR <pratyushrg@gmail.com>
2024-03-28 01:40:03 -0400

    changing signup modules

--- appresources/signup.py
+++ appresources/signup.py
@@ -7,8 +7,8 @@ signup_bp = Blueprint('signup', __name__)
 
 @signup_bp.route('/signup', methods=['GET', 'POST'])
 def signup():
+    signup_message = 'Welcome to signup page'
     if request.method == 'POST':
-    
         # If validation is 0, show only email field
         if request.form['validation'] == '0':
             email = request.form['email']
@@ -21,7 +21,8 @@ def signup():
             # Make a POST request to the login API endpoint
             response = requests.post(current_app.config['global_variables']['url']+'/identityadd', data=json_data, headers=headers).json()
             if response['statusCode']==404:
-                return render_template('signup.html', validation=1 , email = request.form['email'])
+                signup_message = 'Verifaction mail sent to email to verify the email address '+data['email']+' Please click verify to resend vrification link again'
+                return render_template('signup.html', validation=1 , email = request.form['email'], message = signup_message)
         if request.form['validation'] == '1':
             email = request.form['email']
             data = {
@@ -33,9 +34,12 @@ def signup():
             # Make a POST request to the login API endpoint
             response = requests.post(current_app.config['global_variables']['url']+'/identityadd', data=json_data, headers=headers).json()
             if response['statusCode']==404:
-                return render_template('signup.html', validation=1 , email = request.form['email'])
+                signup_message = 'Verifaction mail sent to email to verify the email address '+ data['email']+' Please click verify to resend vrification link again'
+                return render_template('signup.html', validation=1 , email = request.form['email'], message = signup_message)
             else:
-                return render_template('signup.html', validation=2 ,email = request.form['email'])
+                signup_message = 'Email Verification completed please enter username and password'
+                #  \n Password Policy \n Contains at least 1 number \n Contains at least 1 special character \n Contains at least 1 uppercase letter \nContains at least 1 lowercase letter'
+                return render_template('signup.html', validation=2 ,email = request.form['email'], message = signup_message)
         else:
             data = {
                 "email": request.form.get('email'),
@@ -45,10 +49,12 @@ def signup():
             json_data = json.dumps(data)
             headers = {'Content-Type': 'application/json'}
             response = requests.post(current_app.config['global_variables']['url']+'/identityadd', data=json_data, headers=headers).json()
+            
             if response['statusCode'] == 200:
-                return render_template('login.html')
+                return render_template('login.html', error = 'Signup link sent to email '+request.form.get('email')+'plase confirm the link to signin ')
             else:
-                return render_template('signup.html', validation=2 ,email = request.form['email'])
+                signup_message = signup_message = 'Please re-enter a  different username or a strong password'
+                return render_template('signup.html', validation=2 ,email = request.form['email'], message = signup_message)
     
     # Render the signup form
-    return render_template('signup.html', validation=0)
+    return render_template('signup.html', validation=0, message = signup_message)
